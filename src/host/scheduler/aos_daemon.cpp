3ee399110c3d2d518c919d4f94222823b26e9341
Ahmed Khawaja <ahmedkhawaja91@gmail.com>
2019-08-20 01:35:14 +0000

    Host scheduler now accepts FPGA num and JSON file as command line args

--- src/host/scheduler/aos_daemon.cpp
+++ src/host/scheduler/aos_daemon.cpp
@@ -1,68 +1,70 @@
 #include "aos_daemon.h"
 
-int main(void) {
-        
-        bool initFPGA = true;
-        uint64_t num_fpga = 1;
+int main(int argc, char *argv[]) {
 
-        /* Our process ID and Session ID */
-        pid_t pid, sid;
-        
-        /* Fork off the parent process */
-        pid = fork();
-        if (pid < 0) {
-            printf("Error - Unable to fork\n");
-            exit(EXIT_FAILURE);
-        }
-        /* If we got a good PID, then
-           we can exit the parent process. */
-        if (pid > 0) {
-            printf("Exiting parent\n");
-            printf("Daemon pid is %d\n", pid);
-            exit(EXIT_SUCCESS);
-        }
-
-        /* Change the file mode mask */
-        umask(0);
-                
-        /* Open any logs here */        
-                
-        /* Create a new SID for the child process */
-        sid = setsid();
-        if (sid < 0) {  
-            /* Log the failure */
-            printf("Error - Unable to setsid\n");
-            exit(EXIT_FAILURE);
-        }
-
-        /* Change the current working directory */
-        if ((chdir("/")) < 0) {
-            /* Log the failure */
-            printf("Error - Unable to chdir\n");
-            exit(EXIT_FAILURE);
-        }
-        
-        /* Close out the standard file descriptors */
-        //close(STDIN_FILENO);
-        //close(STDOUT_FILENO);
-        //close(STDERR_FILENO);
-
-        // Intialize control over the FPGA
-        aos_host fpga_handle = aos_host(num_fpga, !initFPGA);
-
-        fpga_handle.parseImagesJson("/home/centos/src/project_data/amorphos-design/f1_host/fpga_images.json");
-
-        if (initFPGA) {
-            for (uint64_t fpga_id = 0; fpga_id < num_fpga; fpga_id++) {
-                fpga_handle.loadDefaultImage(fpga_id);
-            }
-        }
-
-        fpga_handle.init_socket();
-
-        // Main loop
-        fpga_handle.listen_loop();
-
+    if (argc != 3) {
+        printf("Usage: ./aos_host_sched <num_fpga> <fpga_images_json>");
         exit(EXIT_SUCCESS);
+    }
+
+    uint64_t num_fpga = std::stoull(argv[1]);
+    std::string jsonFile = argv[2];
+
+    bool initFPGA = true;
+
+    /* Our process ID and Session ID */
+    pid_t pid, sid;
+    
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+        printf("Error - Unable to fork\n");
+        exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then
+       we can exit the parent process. */
+    if (pid > 0) {
+        printf("Exiting parent\n");
+        printf("Daemon pid is %d\n", pid);
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Change the file mode mask */
+    umask(0);
+            
+    /* Open any logs here */        
+            
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0) {  
+        /* Log the failure */
+        printf("Error - Unable to setsid\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory */
+    if ((chdir("/")) < 0) {
+        /* Log the failure */
+        printf("Error - Unable to chdir\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Intialize control over the FPGA
+    aos_host fpga_handle = aos_host(num_fpga, !initFPGA);
+
+    fpga_handle.parseImagesJson(jsonFile);
+
+    if (initFPGA) {
+        for (uint64_t fpga_id = 0; fpga_id < num_fpga; fpga_id++) {
+            fpga_handle.loadDefaultImage(fpga_id);
+        }
+    }
+
+    fpga_handle.init_socket();
+
+    // Main loop
+    fpga_handle.listen_loop();
+
+    exit(EXIT_SUCCESS);
 
 }
